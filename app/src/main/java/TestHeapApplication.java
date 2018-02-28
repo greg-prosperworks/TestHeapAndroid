c517521df38b63af93f71ffdde2f8f7d161bd4a5
gmctaggart <greg@prosperworks.com>
2018-02-28 14:28:25 -0800

    initial commit

---
+++ app/src/main/java/TestHeapApplication.java
@@ -0,0 +1,20 @@
+import android.app.Application;
+
+import com.heapanalytics.android.Heap;
+
+/**
+ * Created by greg on 2/28/18.
+ */
+
+public class TestHeapApplication extends Application {
+
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//
+//
+//        // Heap does not initialize, but does not give "W/Heap: Could not find BuildConfig"
+////        Heap.init(getApplicationContext(), "2572874845");
+////        Heap.init(this, "2572874845");
+//    }
+}
